41ab18af84ef7dec5bd61235a8cd80656c6024da
fikiaprian23 <fikiaprian23@gmail.com>
2023-11-24 21:45:15 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Route from './components/Route/Route'
+import { RouterProvider } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={Route} />
+    </>
+  )
+}
+
+export default App
