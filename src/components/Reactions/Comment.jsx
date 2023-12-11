cad76dcabc17d798ead65d8aa9ad2f9f276f0273
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-11 14:39:16 +0700

    delete dark mode

--- src/components/Reactions/Comment.jsx
+++ src/components/Reactions/Comment.jsx
@@ -1,21 +1,21 @@
 const Comment = () => {
   return (
-    <section className="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
+    <section className="bg-white  py-8 lg:py-16 antialiased">
       <div className="max-w-2xl mx-auto px-4">
         <div className="flex justify-between items-center mb-6">
-          <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
+          <h2 className="text-lg lg:text-2xl font-bold text-gray-900 ">
             Discussion (20)
           </h2>
         </div>
         <form className="mb-6">
-          <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+          <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200  ">
             <label htmlFor="comment" className="sr-only">
               Your comment
             </label>
             <textarea
               id="comment"
               rows="6"
-              className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+              className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none  dark:placeholder-gray-400 "
               placeholder="Write a comment..."
               required
             ></textarea>
@@ -27,10 +27,10 @@ const Comment = () => {
             Post comment
           </button>
         </form>
-        <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+        <article className="p-6 text-base bg-white rounded-lg ">
           <footer className="flex justify-between items-center mb-2">
             <div className="flex items-center">
-              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900  font-semibold">
                 <img
                   className="mr-2 w-6 h-6 rounded-full"
                   src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
@@ -47,7 +47,7 @@ const Comment = () => {
             <button
               id="dropdownComment1Button"
               data-dropdown-toggle="dropdownComment1"
-              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50  dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               type="button"
             >
               <svg
@@ -127,10 +127,10 @@ const Comment = () => {
             </button>
           </div>
         </article>
-        <article className="p-6 mb-3 ml-6 lg:ml-12 text-base bg-white rounded-lg dark:bg-gray-900">
+        <article className="p-6 mb-3 ml-6 lg:ml-12 text-base bg-white rounded-lg ">
           <footer className="flex justify-between items-center mb-2">
             <div className="flex items-center">
-              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900  font-semibold">
                 <img
                   className="mr-2 w-6 h-6 rounded-full"
                   src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
@@ -147,7 +147,7 @@ const Comment = () => {
             <button
               id="dropdownComment2Button"
               data-dropdown-toggle="dropdownComment2"
-              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50  dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               type="button"
             >
               <svg
@@ -224,10 +224,10 @@ const Comment = () => {
             </button>
           </div>
         </article>
-        <article className="p-6 mb-3 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+        <article className="p-6 mb-3 text-base bg-white border-t border-gray-200  ">
           <footer className="flex justify-between items-center mb-2">
             <div className="flex items-center">
-              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900  font-semibold">
                 <img
                   className="mr-2 w-6 h-6 rounded-full"
                   src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
@@ -244,7 +244,7 @@ const Comment = () => {
             <button
               id="dropdownComment3Button"
               data-dropdown-toggle="dropdownComment3"
-              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50  dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               type="button"
             >
               <svg
@@ -322,10 +322,10 @@ const Comment = () => {
             </button>
           </div>
         </article>
-        <article className="p-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+        <article className="p-6 text-base bg-white border-t border-gray-200  ">
           <footer className="flex justify-between items-center mb-2">
             <div className="flex items-center">
-              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900  font-semibold">
                 <img
                   className="mr-2 w-6 h-6 rounded-full"
                   src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
@@ -342,7 +342,7 @@ const Comment = () => {
             <button
               id="dropdownComment4Button"
               data-dropdown-toggle="dropdownComment4"
-              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50  dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               type="button"
             >
               <svg
